b2a52718b7e4dfb283a68279d1134085f82d3401
Enrico Tassi <Enrico.Tassi@Inria.fr>
2022-11-14 16:07:32 +0100

    x

--- go.sh
+++ go.sh
@@ -2,6 +2,7 @@
 set -e
 set -x
 
+# ocaml-wasm wants 4.12
 export OPAMROOT=$PWD/opam
 [ -d opam ] || (opam init --bare -y && opam switch create wacoq --packages ocaml-variants.4.12.0+options,ocaml-option-32bit -y)
 eval $(opam env --switch=wacoq --set-switch)
@@ -13,19 +14,55 @@ tar -xJf node-$NODE-linux-x64.tar.xz
 export PATH=$PWD/node-$NODE-linux-x64/bin:$PATH
 which node
 
+# Wasi 12 is the one tested for wacoq 8.15
 WASI=12.0
 [ -f wasi-sdk-$WASI-linux.tar.gz ] || wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-12/wasi-sdk-$WASI-linux.tar.gz
 tar -xzf wasi-sdk-$WASI-linux.tar.gz
 export WASI_SDK_PATH=$PWD/wasi-sdk-$WASI
 
-[ -d wacoq-bin ] || (git clone git@github.com:gares/wacoq-bin.git -b v8.15 --recursive && cd wacoq-bin && npm install && make bootstrap && make coq)
-[ -f wacoq-bin/wacoq-bin-0.15.1.tar.gz ] || (cd wacoq-bin && make wacoq && make dist-npm && make install)
+CLEAN=true
 
-[ -d addons ] || (git clone git@github.com:gares/addons.git --recursive && cd addons && npm install jquery jszip ../wacoq-bin/wacoq-bin-0.15.1.tar.gz)
-[ -f addons/_build/wacoq/jscoq-mathcomp-0.16.0.tgz ] || (cd addons && make world && make pack)
+# Wacoq backend (includes coq)
+[ -d wacoq-bin ] || CLEAN=false
+$CLEAN || (rm -rf wacoq-bin ; git clone git@github.com:gares/wacoq-bin.git -b v8.15 --recursive && cd wacoq-bin && npm install && make bootstrap && make coq)
 
-[ -d jscoq ] || (git clone git@github.com:gares/jscoq.git --recursive -b v8.15 && cd jscoq && npm install ../wacoq-bin/wacoq-bin-0.15.1.tar.gz ../addons/_build/wacoq/*tgz)
-[ -f wacoq-0.15.1.tgz ] || (cd jscoq && make wacoq && make dist-npm-wacoq)
+[ -f wacoq-bin/wacoq-bin-0.15.1.tar.gz ] || CLEAN=false
+$CLEAN || (cd wacoq-bin && make wacoq && make dist-npm)
 
+[ -f $OPAMROOT/wacoq/bin/coqc ] || CLEAN=false
+$CLEAN || (cd wacoq-bin && make install)
+
+# Wacoq libs
+# [ -d addons ] || CLEAN=false
+# $CLEAN || (rm -rf addons; git clone git@github.com:gares/addons.git --recursive && cd addons && npm install jquery jszip ../wacoq-bin/wacoq-bin-0.15.1.tar.gz)
+# 
+# [ -f addons/_build/wacoq/wacoq-elpi-0.15.0.tgz ] || CLEAN=false
+# $CLEAN || (cd addons && make elpi)
+# 
+# [ -f addons/_build/wacoq/wacoq-hierarchy-builder-0.15.0.tgz ] || CLEAN=false
+# $CLEAN || (cd addons && make hierarchy-builder)
+# 
+# [ -f addons/_build/wacoq/wacoq-mathcomp-0.15.0.tgz ] || CLEAN=false
+# $CLEAN || (cd addons && make mathcomp)
+# 
+# $CLEAN || (cd addons && make pack)
+
+# Wacoq frontend
+[ -d jscoq ] || CLEAN=false
+$CLEAN || (rm -rf jscoq; git clone git@github.com:gares/jscoq.git --recursive -b v8.15 && cd jscoq && npm install ../wacoq-bin/wacoq-bin-0.15.1.tar.gz ../addons/_build/wacoq/*tgz)
+
+[ -f jscoq/wacoq-0.15.1.tgz ] || CLEAN=false
+$CLEAN || (cd jscoq && make wacoq && make dist-npm-wacoq)
+
+# Local deploy
 cd deploy
-npm install ../jscoq/wacoq-0.15.1.tgz ../addons/_build/wacoq/*tgz
+rm -rf node_modules
+npm install ../wacoq-bin/wacoq-bin-0.15.1.tar.gz
+npm install ../jscoq/wacoq-0.15.1.tgz
+npm install ../addons/_build/wacoq/wacoq-elpi-0.15.0.tgz
+npm install ../addons/_build/wacoq/wacoq-hierarchy-builder-0.15.0.tgz
+npm install ../addons/_build/wacoq/wacoq-mathcomp-0.15.0.tgz
+python3 -m http.server
+
+
+# & google-chrome --allow-file-access-from-files --js-flags="--harmony-tailcalls" --js-flags="--stack-size=65536" ./index.html
